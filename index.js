64173b1b87fb3f277d78a18a5d3d3e9ba3f88bef
3imed-jaberi <imed_jebari@hotmail.fr>
2020-06-14 02:30:08 +0200

    update src code: async/await over generator + Koa 2 way 🚀 ..

--- index.js
+++ index.js
@@ -1,39 +1,28 @@
-/*!
- * snapshot - index.js
- * Copyright(c) 2014 dead_horse <dead_horse@qq.com>
- * MIT Licensed
- */
-
 'use strict';
 
-/**
- * Module dependencies.
- */
-
-var copy = require('copy-to');
-
-var defaultOptions = {
-  max: 50 * 1024 * 1024,  // 50mb
-  maxAge: 12 * 60 * 60 * 1000,  // 12h
-  length: function (n) {
+const DefaultCache = require('lru-cache');
+const defaultOptions = {
+  max: 50 * 1024 * 1024, // 50mb
+  maxAge: 12 * 60 * 60 * 1000, // 12h
+  length(n) {
     return n.body ? n.body.length : 0;
   }
 };
 
 module.exports = function (options) {
-  options = options || {};
-  copy(defaultOptions).to(options);
+  options = options ? { ...options, ...defaultOptions } : defaultOptions;
+  const cache = options.cache || new DefaultCache(options);
 
-  var cache = options.cache || require('lru-cache')(options);
-
-  return function* snapshot(next) {
+  // eslint-disable-next-line func-names
+  return async function snapshot(ctx, next) {
     try {
-      yield *next;
+      await next();
     } catch (err) {
-      return errorHandler(err, this);
+      return errorHandler(err, ctx);
     }
-    getFromCache(this);
-    setCache(this);
+
+    getFromCache(ctx);
+    setCache(ctx);
   };
 
   function setCache(ctx) {
@@ -41,7 +30,7 @@ module.exports = function (options) {
     // only cache html
     if (!ctx.response.is('html')) return;
     // only cache 2xx
-    if ((ctx.status / 100 | 0) !== 2) return;
+    if (((ctx.status / 100) | 0) !== 2) return;
     // update cache
     cache.set(ctx.path, {
       body: ctx.body,
@@ -52,27 +41,24 @@ module.exports = function (options) {
   function getFromCache(ctx) {
     if (ctx.noSnapshot) return;
     if (ctx.status < 500) return;
-    var c = cache.get(ctx.path);
+    const c = cache.get(ctx.path);
     if (!c) return;
 
     ctx.type = c.type;
     ctx.body = c.body;
     ctx.set('X-Snapshot-Status', ctx.status || 500);
     ctx.status = 200;
-    return;
   }
 
   function errorHandler(err, ctx) {
     if (ctx.noSnapshot) throw err;
     if (err.status && err.status < 500) throw err;
-    var c = cache.get(ctx.path);
+    const c = cache.get(ctx.path);
     if (!c) throw err;
 
     ctx.app.emit('error', err);
     ctx.type = c.type;
     ctx.body = c.body;
     ctx.set('X-Snapshot-Status', err.status || 500);
-    return;
   }
-
 };
